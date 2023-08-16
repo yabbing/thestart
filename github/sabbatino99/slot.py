d67c003f67fc3ff6891401a83478a04fee427886
yabbing <sabbatino99@gmail.com>
2023-08-16 18:39:38 -0400

    stop1

--- github/sabbatino99/slot.py
+++ github/sabbatino99/slot.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposit():
     while True:
         amount = input('What would you like to deposit $')
@@ -6,8 +10,46 @@ def deposit():
             if amount > 0:
                 break
             else:
-                print('Amount must be great than 0.')
+                print('Amount must be greater than 0.')
         else:
-            print('Please Enter a number.')
-    
+            print('Please Enter a number.')    
     return amount
+
+
+def get_number_of_lines():
+    while True:
+        lines = input('How many lines would you like to bet on? 1-' + str(MAX_LINES) + ')? ')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print('Enter a valid number of lines.')
+        else:
+            print('Please Enter a number.')    
+    return lines
+
+def get_bet():
+    while True:
+        amount = input('What would you like to bet on each line? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f'Amount must be between ${MIN_BET} - ${MAX_BET}.')
+        else:
+            print('Please Enter a number.')    
+    return amount
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+    
+    
+    print(f'You are betting ${bet} on {lines} lines. The Total bet is: ${total_bet}')
+main()
