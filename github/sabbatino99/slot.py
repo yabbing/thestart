60cf4066b0cac36e3448d51863fae718599ddc9c
yabbing <sabbatino99@gmail.com>
2023-08-16 16:32:39 -0400

    start

--- github/sabbatino99/slot.py
+++ github/sabbatino99/slot.py
@@ -1,1 +1,13 @@
-print("why dont this shit work")
+def deposit():
+    while True:
+        amount = input('What would you like to deposit $')
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print('Amount must be great than 0.')
+        else:
+            print('Please Enter a number.')
+    
+    return amount
