525c85ef345017b20ab648cf2608a4e515db530e
yabbing <sabbatino99@gmail.com>
2023-09-16 13:25:22 -0400

    fixed

--- github/sabbatino99/Dice_game.py
+++ github/sabbatino99/Dice_game.py
@@ -8,4 +8,7 @@ while roll_again == 'yes' or roll_again == 'y':
     value1 = random.choice(dice)
     value2 = random.choice(dice)
     print(value1, value2)
-roll_again = input('Press Y or yes to roll dices again.')
+    
+
+    roll_again = input('Press Y or yes to roll dices again.')
+    pass
