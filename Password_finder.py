9bb4f23c32fd0248f0d1da7f734bc8e8ce16352d
Sab <130924535+yabbing@users.noreply.github.com>
2024-01-11 18:06:37 -0500

    done now

--- Password_finder.py
+++ Password_finder.py
@@ -6,30 +6,30 @@ def cryptBytes (hash_type, salt, value) :
     if not hash_type:
         hash_type ="SHA"
     if not salt:
-        salt = base64.urlsafe_b64encode (os. urandom (16)).decode ('utf-8' )
-    hash_obj = hashlib. new(hash_type)
-    hash_obj.update(salt. encode ('utf-8'))
+        salt = base64.urlsafe_b64encode (os. urandom (16)).decode ('utf-8')
+    hash_obj = hashlib.new(hash_type)
+    hash_obj.update(salt. encode('utf-8'))
     hash_obj.update(value)
     hashed_bytes = hash_obj.digest()
-    result = f"$(hash_type)${salt}${base64.urlsafe_b64encode(hashed_bytes).decode('utf-8'). replace('+', '')}"
+    result = f"${hash_type}${salt}${base64.urlsafe_b64encode(hashed_bytes).decode('utf-8').replace('+', '.')}"
     return result
-def getCryptedBytes (hash_type, salt, value) :
+def getCryptedBytes(hash_type, salt, value):
     try:
-        hash_obj= hashlib. new(hash_type)
-        hash_obj.update(salt.encode('utf-8' ))
-        hash_obj.update (value)
+        hash_obj= hashlib.new(hash_type)
+        hash_obj.update(salt.encode('utf-8'))
+        hash_obj.update(value)
         hashed_bytes = hash_obj.digest()
-        return base64. urlsafe_b64encode (hashed_bytes).decode ('utf-8'). replace('+', '.')
+        return base64.urlsafe_b64encode(hashed_bytes).decode('utf-8').replace('+', '.')
     except hashlib.NoSuchAlgorithmException as e:
         raise Exception(f"Error while computing hash of type {hash_type}: {e}")
-hash_type = "SHAl"
+hash_type = "SHA1"
 salt = "d"
 search = "$SHAl$d$uP0_QaVBpDWFeo8-dRzDqRwXQ2I="
 wordlist = '/usr/share/wordlists/rockyou.txt'
 with open(wordlist,'r', encoding='latin-1') as password_list:
     for password in password_list:
         value = password.strip()
-        hashed_password = cryptBytes (hash_type, salt, value. encode ('utf-8'))
+        hashed_password = cryptBytes(hash_type, salt, value. encode ('utf-8'))
         # print(hashed password)
         if hashed_password == search:
             print(f'Found Password:{value}, hash:{hashed_password}')
